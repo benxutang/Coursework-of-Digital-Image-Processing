87924b29fbb10a2862b8f867820164dd2c7b19e4
mookie1702 <54575918+mookie1702@users.noreply.github.com>
2020-12-08 07:39:07 -0600

    V3.0.1

--- H_detector/src/detector.cpp
+++ H_detector/src/detector.cpp
@@ -2,56 +2,119 @@
 
 using namespace cv;
 using namespace std;
+using namespace message_filters;
 
-int main(int argc, char*argv[])
-{
-    VideoCapture capture;
-    capture.open(0); // 1 为打开 zed 相机, 0 为打开笔记本摄像头
-    ROS_WARN("*****START");
-    ros::init(argc,argv,"trafficLaneTrack");//初始化 ROS 节点
+class SubscribeAndPublish{
+public:
     ros::NodeHandle n;
-    ros::Rate loop_rate(10);//定义速度发布频率
-    ros::Publisher pub = n.advertise<geometry_msgs::Twist>("/smoother_cmd_vel", 5);
-
-    if (!capture.isOpened())
-    {
-        printf("摄像头没有正常打开,重新插拔工控机上当摄像头\n");
-        return 0;
+    ros::Publisher pub;
+    SubscribeAndPublish()    {
+        ros::Rate loop_rate(10);
+        //pub = n.advertise<geometry_msgs::Twist>("/controler", 5);
+        pub = n.advertise<geometry_msgs::Twist>("/smoother_cmd_vel", 5);
+        message_filters::Subscriber<sensor_msgs::Image> sub1(n, "/zed/zed_node/rgb_raw/image_raw_color", 1);
+        message_filters::Subscriber<sensor_msgs::Image> sub2(n, "/zed/zed_node/depth/depth_registered", 1);
+        
+        typedef sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image> syncPolicy;
+        Synchronizer<syncPolicy> sync(syncPolicy(10), sub1, sub2);
+        sync.registerCallback(boost::bind(&SubscribeAndPublish::messagecallback, this, _1, _2));
+        ros::spin();
     }
-    waitKey(1000);
-
-    Mat frame_zed, frame_left, frame_right;//当前帧图片
-//    Mat skeleton;
-
-    int hough_thresh = HOUGH_THRESH_INIT;
-
-    while (ros::ok())
+    void messagecallback(const sensor_msgs::Image::ConstPtr &msg1, const sensor_msgs::Image::ConstPtr &msg2)
     {
-        capture.read(frame_zed);
-        if(frame_zed.empty())
+        static int count = 0;
+        static vector<Point2f> Angle;
+        static clock_t begin;
+        static bool checkonce = true;
+        static double angle1 = 0, angle2 = 0;
+        static clock_t runtime;
+        static double duration;
+        static int centerIdx;
+        static double depth_data;
+
+        cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg1, sensor_msgs::image_encodings::BGR8);
+        Mat frame_left = cv_ptr->image;
+        Rect rect;
+
+        float *depths = (float *)(&msg2->data[0]); // 定义照片深度信息
+
+        int hough_thresh = HOUGH_THRESH_INIT;
+
+        //Gain Container
+        if(count < 50)
         {
-            break;
+            vector<double> LineScope;
+            DetectH(frame_left, H_MIN_LEFT, H_MAX_LEFT, 0,
+                    hough_thresh, rect, LineScope);
+            //DetectH(frame_right, H_MIN_RIGHT, H_MAX_RIGHT, 1);
+            if (LineScope.size() > 1)
+            {
+                SwapVector2(LineScope);
+                Point2f angle;
+                angle.x = LineScope[0];
+                angle.y = LineScope[1];
+                if (abs(angle.x - angle.y) > 20)
+                {
+                    Angle.push_back(angle);
+                }
+            }
+
+            centerIdx = rect.x + msg2->width * rect.y;
+            depth_data = depths[centerIdx];
+            cout<<rect.x<<"  "<<rect.y<<"  "<<depth_data<<endl;
+            waitKey(2);
+            count++;
         }
 
-        //截取zed的左目图片
-        frame_left = frame_zed(cv::Rect(0, 0, frame_zed.cols / 2, frame_zed.rows));
-        frame_right = frame_zed(cv::Rect(frame_zed.cols/2, 0, frame_zed.cols / 2, frame_zed.rows));
+        else
+        {            
+            if (checkonce == true)
+            {
+                //Averaging
+                for (size_t i = 0; i < Angle.size(); i++)
+                {
+                    angle1 += Angle[i].x / Angle.size();
+                    angle2 += Angle[i].y / Angle.size();
+                }
+                begin = clock();
+                cout << angle1 << "  " << angle2 << endl;
+                checkonce = false;
+            }
 
-        DetectH(frame_left, H_MIN_LEFT, H_MAX_LEFT, 0, hough_thresh);
-        //DetectH(frame_right, H_MIN_RIGHT, H_MAX_RIGHT, 1);
+            runtime = clock();
+            duration = (double)(runtime - begin) / CLOCKS_PER_SEC;
+            geometry_msgs::Twist cmd_red;
+            cout<<depth_data<<endl;
+            if(duration <= (depth_data/LINEAR_X/10))
+            {
+                cmd_red.linear.x = LINEAR_X;
+                cmd_red.angular.z = 0;
+                pub.publish(cmd_red);
+            }
 
-        geometry_msgs::Twist cmd_red;
-        // 车的速度值设置
-        cmd_red.linear.x = LINEAR_X;
-        cmd_red.linear.y = 0;
-        cmd_red.linear.z = 0;
-        cmd_red.angular.x = 0;
-        cmd_red.angular.y = 0;
-        cmd_red.angular.z = ANGULAR;
-        pub.publish(cmd_red);
+            else if ((depth_data/LINEAR_X/10) < duration && duration <= ((depth_data/LINEAR_X/10) + (180 + angle1) * 25 / 3600))
+            {
+                //cout<<angle1<<endl;
+                cmd_red.linear.x = 0;
+                cmd_red.angular.z = ANGULAR;
+                pub.publish(cmd_red);
+            }
 
-        ros::spinOnce();
-        waitKey(5);
+            else
+            {
+                cout<<duration<<endl;
+                cmd_red.linear.x = 0;
+                cmd_red.angular.z = 0;
+                pub.publish(cmd_red);
+            }            
+        }
     }
+};
+
+int main(int argc, char **argv)
+{
+    ROS_WARN("*****START*****");
+    ros::init(argc, argv, "trafficLaneTrack");
+    SubscribeAndPublish sub_and_pub;
     return 0;
 }
