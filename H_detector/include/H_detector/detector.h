459101648ec9724db1ef01a694c5ebec93a200b4
mookie1702 <54575918+mookie1702@users.noreply.github.com>
2020-12-08 06:36:41 -0600

    2.0.1

--- H_detector/include/H_detector/detector.h
+++ H_detector/include/H_detector/detector.h
@@ -7,17 +7,27 @@
 #include <highgui.h>
 #include <opencv2/opencv.hpp>
 #include <opencv2/core/core.hpp>
+#include <boost/thread/thread.hpp>
+
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/exact_time.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <message_filters/time_synchronizer.h>
+
+#include <cv_bridge/cv_bridge.h>
+#include <image_transport/image_transport.h>
 
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include "std_msgs/Bool.h"
 #include "std_msgs/Float32.h"
-#include<geometry_msgs/Twist.h>
+#include <geometry_msgs/Twist.h>
 #include "sensor_msgs/Image.h"
 
 #define max_corners 4
 #define LINEAR_X 0
-#define ANGULAR 0
+#define ANGULAR -0.2
 
 #define C CV_PI /3
 #define CANNY_EDGE_THRESH 1
@@ -27,11 +37,13 @@
 #define H_MIN_RIGHT 75
 #define H_MAX_RIGHT 90
 #define HOUGH_THRESH_INIT 8
+#define ANG_RUNTIME 10
 
 using namespace cv;
 using namespace std;
 
-void DetectH(Mat& frame, int h_min, int h_max, bool _side_flag, int& hough_thresh);
+void DetectH(Mat& frame, int h_min, int h_max, bool _side_flag,
+        int& hough_thresh, Rect& rect, vector<double>& LineScope);
 
 vector<Point2f> bubbleSort(vector<Point2f>& intn, const Rect& rect);
 
@@ -50,3 +62,5 @@ void LineThresh(vector<Vec2f>& lines);
 int CheckLine(vector<Vec4i>& lines);
 
 void GetLinesScope(const vector<Vec4i>& lines, vector<double>& Scope);
+
+void SwapVector2(vector<double>& Vector);
